e3e4b5c90e56ccb11894f3622cc88db206e84ab7
Alfred Sopia <sopiaah100@gmail.com>
2023-05-06 20:28:39 +0300

    Initial commit

---
+++ src/main/java/com/sovtech/starwars/graphql/StarwarsGraphqlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sovtech.starwars.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsGraphqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsGraphqlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sovtech/starwars/graphql/StarwarsGraphqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovtech.starwars.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsGraphqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
