0ca80e590b4f649f14d181609d37681c3fd1b915
Alfred Sopia <sopiaah100@gmail.com>
2023-05-07 19:25:09 +0300

    Add logging functionality to the application
    This commit adds logging functionality to the application to log all requests, responses, and authentication activity. A LoggingFilter has been implemented to log incoming requests and outgoing responses, including a unique request ID for each request. The logs are printed on the terminal for easy access and debugging. This will improve the visibility and traceability of the application, making it easier to identify and troubleshoot issues.

--- src/main/java/com/sovtech/starwars/graphql/service/impl/StarWarsServiceImpl.java
+++ src/main/java/com/sovtech/starwars/graphql/service/impl/StarWarsServiceImpl.java
@@ -1,8 +1,10 @@
 package com.sovtech.starwars.graphql.service.impl;
 
+import com.sovtech.starwars.graphql.config.LoggingFilter;
 import com.sovtech.starwars.graphql.data.model.api.response.PeopleResponse;
 import com.sovtech.starwars.graphql.data.model.api.response.PersonResponse;
 import com.sovtech.starwars.graphql.service.StarWarsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -12,41 +14,50 @@ import reactor.core.publisher.Mono;
  * @author asopia
  */
 @Service
+@Slf4j
 public class StarWarsServiceImpl implements StarWarsService {
     private static final String BASE_URL = "https://swapi.dev/api";
 
     private final WebClient webClient;
 
     public StarWarsServiceImpl(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl(BASE_URL).build();
+        this.webClient = webClientBuilder.baseUrl(BASE_URL)
+                .filter(new LoggingFilter()) // Add the logResponse() filter here
+                .build();
     }
 
     @Override
     public Flux<PersonResponse> getAllPeople(int page) {
+        log.info("Request received to retrieve all people in page={}", page);
         return webClient.get()
                 .uri("/people/?page={page}", page)
                 .retrieve()
                 .bodyToMono(PeopleResponse.class)
+                .doOnNext(response -> log.info("Retrieved all people in page={} | previousPage={} | nextPage={}", page, response.getPrevious(),response.getNext()))
                 .flatMapIterable(PeopleResponse::getResults)
                 .map(PeopleResponse::mapToPerson);
     }
 
     @Override
     public Flux<PersonResponse> searchPerson(String name) {
+        log.info("Request received to search all people with name like={}", name);
         return webClient.get()
                 .uri("/people/?search={name}", name)
                 .retrieve()
                 .bodyToMono(PeopleResponse.class)
+                .doOnNext(response -> log.info("Retrieved all persons with name like={}", name))
                 .flatMapIterable(PeopleResponse::getResults)
                 .map(PeopleResponse::mapToPerson);
     }
 
     @Override
     public Mono<PersonResponse> getPerson(String name) {
+        log.info("Request received to retrieve a person with name={}", name);
         return webClient.get()
                 .uri("/people/?search={name}", name)
                 .retrieve()
                 .bodyToMono(PeopleResponse.class)
+                .doOnNext(response -> log.info("Retrieved a person named={}", name))
                 .flatMapIterable(PeopleResponse::getResults)
                 .next()
                 .map(PeopleResponse::mapToPerson);
