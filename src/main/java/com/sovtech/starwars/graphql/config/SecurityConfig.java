549389d64fe2410cdf6728c890d447e687fbfa71
Alfred Sopia <sopiaah100@gmail.com>
2023-05-07 17:51:42 +0300

    Refactor SecurityConfig for CORS configuration
    Remove the CorsConfig class and configure CORS directly in the SecurityConfig class using the CorsConfigurationSource interface. Allow requests from localhost:4200 and set the allowed methods to POST. Also, add the Authorization header to the response to allow basic authentication requests.

--- src/main/java/com/sovtech/starwars/graphql/config/SecurityConfig.java
+++ src/main/java/com/sovtech/starwars/graphql/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package com.sovtech.starwars.graphql.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,32 +25,47 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
+
     @Bean
-    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf().disable()
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().authenticated()
-                )
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.POST).authenticated()
+                .anyExchange().authenticated()
+                .and()
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("POST"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("sovtech")
+                .password(passwordEncoder().encode("sovtech"))
+                .roles()
+                .build();
+        return new MapReactiveUserDetailsService(user);
     }
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public static MapReactiveUserDetailsService userDetailsService() {
-        UserDetails admin = User.builder()
-                .username("sovtech")
-                .password(passwordEncoder().encode("sovtech"))
-                .roles("ADMIN")
-                .build();
-        return new MapReactiveUserDetailsService(admin);
-    }
 }
-
-
-
